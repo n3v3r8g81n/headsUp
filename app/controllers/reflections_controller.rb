96c3ec90de802941ea99073a834622b887d97c08
Laur dArmien <n3v3r8g81n@gmail.com>
2019-09-05 23:18:15 +0000

    restored index page to do reverse_order. removed from controller.

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -1,6 +1,8 @@
 class ReflectionsController < ApplicationController
     def index
         # @reflections = Reflection.all
-        @reflections = Reflection.order(:created_at).reverse_order.paginate(:page => params[:page], :per_page=>3)
+        # .paginate(:page => params[:page], :per_page=>3)
+        @reflections = Reflection.all
+
     end
 end
