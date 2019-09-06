e9ab0383f975b96b388bab19364b6f13920c6de2
Laur dArmien <n3v3r8g81n@gmail.com>
2019-09-06 20:38:36 +0000

    changed text color

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -1,8 +1,9 @@
 class ReflectionsController < ApplicationController
+
     def index
         # @reflections = Reflection.all
         # .paginate(:page => params[:page], :per_page=>3)
         @reflections = Reflection.all
-
     end
+    
 end
